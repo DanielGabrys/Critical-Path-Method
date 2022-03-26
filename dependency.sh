fb59bf9c1ed589ab48d83dde78a93884ed73f95a
DanielGabrys <daniello.gab@gmail.com>
2022-03-26 15:49:28 +0100

    change structure of executable file

---
+++ dependency.sh
@@ -0,0 +1,1 @@
+sudo -- sh -c 'apt install openjdk-11-jdk; apt install maven' #dependecies
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-sudo -- sh -c 'apt install openjdk-11-jdk; install maven' #dependecies
-java -jar target/CPM-1.0-SNAPSHOT-shaded.jar #program
+java -jar CPM.jar #program
